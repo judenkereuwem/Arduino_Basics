16dbb71070a87090c8c5bc9ab0034c9322f5f59c
unknown <judenkereuwem@gmail.com>
2022-10-21 16:48:08 +0100

    voltage sensor

--- arduino_LED_brightness_potentiometer/arduino_LED_brightness_potentiometer.ino
+++ arduino_LED_brightness_potentiometer/arduino_LED_brightness_potentiometer.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int potPin = A0;
+const int ledPin = 6;
+int led_value;
+int pot_value;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // reading from potentiometer
+  pot_value = analogRead(potPin);
 
+  // Mapping the Values between 0 to 255 
+  led_value = map(pot_value, 0, 1023, 0, 255);
+
+  // fade the LED using the calibrated value:
+  analogWrite(ledPin, led_value);
 }
---
+++ arduino_voltage_sensor/arduino_voltage_sensor.ino
@@ -0,0 +1,34 @@
+/*
+ * This progam uses B25 Voltage sensor connected 
+ * to Arduino board to read voltage in a circuit 
+ * and display results on Serial monitor.
+*/
+
+const int voltgeSensor = A0;
+
+float vOUT = 0.0;
+float vIN = 0.0;
+
+float R1 = 30000.0;
+float R2 = 7500.0;
+
+int value = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  value = analogRead(A0);
+  
+  vOUT = (value * 5.0)/1024.0;
+  vIN = vOUT / (R2/(R1+R2));
+
+  //display on serial monitor
+  Serial.print("Voltage = ");
+  Serial.print(vIN, 2);
+  Serial.println(" volts DC");
+
+  delay(1000);
+}
