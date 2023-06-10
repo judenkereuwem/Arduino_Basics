75da443697f3147c3989a821d86514b86dac13f0
unknown <judenkereuwem@gmail.com>
2023-06-10 21:53:28 +0100

    sound sensor

---
+++ arduino_soundSensor/arduino_soundSensor.ino
@@ -0,0 +1,16 @@
+
+const int sensorPin = A0;
+
+void setup() {
+  pinMode(sensorPin, INPUT);  // Set sensor pin as an INPUT
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read Sound sensor
+  int sensorData = analogRead(sensorPin);
+  Serial.print("sensor: ");
+  Serial.println(sensorData/2);
+  delay(100);
+   
+}
